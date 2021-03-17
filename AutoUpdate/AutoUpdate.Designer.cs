ed3e8cc9081a5aebe582c740cd7bf7f1b91c7e82
swgil007 <swgil007@naver.com>
2021-03-17 16:25:29 +0900

    adding upload method

--- AutoUpdate/AutoUpdate.Designer.cs
+++ AutoUpdate/AutoUpdate.Designer.cs
@@ -31,8 +31,10 @@ namespace AutoUpdate
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.version_label = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,41 +51,63 @@ namespace AutoUpdate
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(303, 9);
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(362, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(105, 32);
+            this.label2.Size = new System.Drawing.Size(162, 50);
             this.label2.TabIndex = 2;
             this.label2.Text = "Checker";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(570, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 21);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.Text = "버전 1.0.0";
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(42, 48);
+            this.dataGridView1.Location = new System.Drawing.Point(42, 75);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(602, 208);
+            this.dataGridView1.Size = new System.Drawing.Size(789, 311);
             this.dataGridView1.TabIndex = 4;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "버튼";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(37, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "버전 : ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // version_label
+            // 
+            this.version_label.AutoSize = true;
+            this.version_label.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.version_label.Location = new System.Drawing.Point(95, 51);
+            this.version_label.Name = "version_label";
+            this.version_label.Size = new System.Drawing.Size(0, 21);
+            this.version_label.TabIndex = 7;
             // 
             // AutoUpdate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(691, 318);
+            this.ClientSize = new System.Drawing.Size(880, 438);
+            this.Controls.Add(this.version_label);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "AutoUpdate";
@@ -99,8 +123,10 @@ namespace AutoUpdate
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label version_label;
     }
 }
 
--- AutoUpdate/AutoUpdate.cs
+++ AutoUpdate/AutoUpdate.cs
@@ -9,11 +9,18 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using AutoUpdate;
 using MySql.Data.MySqlClient;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
 
 namespace AutoUpdate
 {
     public partial class AutoUpdate : Form
     {
+        private string ftpId = "checker";
+        private string ftpPwd = "dkfdptmdps404";
+        private string ftpPath = "ftp://192.168.1.23";
+
         public AutoUpdate()
         {
             InitializeComponent();
@@ -30,10 +37,76 @@ namespace AutoUpdate
 
             DataTable table = new DataTable();
 
+            dataGridView1.Columns.Add("c_seq", "C_SEQ");
+
+            dataGridView1.Columns.Add("name", "이름");
+
+            dataGridView1.Columns.Add("version", "버전");
+
+            dataGridView1.Columns.Add("comment", "커멘트");
+
+            dataGridView1.Columns.Add("ip", "작업IP");
+
+            dataGridView1.Columns.Add("path", "경로");
+
+            dataGridView1.Columns.Add("filesize", "크기");
+
+
             dbAdapter.Fill(table);
 
-            dataGridView1.DataSource = table;
+            //dataGridView1.DataSource = table;
 
+            int cnt = 0;
+            foreach (DataRow row in table.Rows)
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[cnt].Cells["c_seq"].Value = row["c_seq"].ToString();
+                dataGridView1.Rows[cnt].Cells["name"].Value = row["name"].ToString();
+                dataGridView1.Rows[cnt].Cells["version"].Value = row["version"].ToString();
+                dataGridView1.Rows[cnt].Cells["comment"].Value = row["comment"].ToString();
+                dataGridView1.Rows[cnt].Cells["ip"].Value = row["ip"].ToString();
+                dataGridView1.Rows[cnt].Cells["path"].Value = row["path"].ToString();
+                dataGridView1.Rows[cnt].Cells["filesize"].Value = row["filesize"].ToString();
+
+                cnt++;
+            }
+
+
+            string versionStr = ReadFileToString("test.txt");
+
+            version_label.Text = versionStr;
+        }
+
+        public string ReadFileToString (string fileName)
+        {
+            fileName = ftpPath + "/" + fileName;
+
+            FtpWebRequest ReqFile = WebRequest.Create(fileName) as FtpWebRequest;
+            ReqFile.Method = WebRequestMethods.Ftp.DownloadFile;
+            ReqFile.Credentials = new NetworkCredential(ftpId, ftpPwd);
+
+            string resDataString = String.Empty;
+
+            using (FtpWebResponse ResponseFile = ReqFile.GetResponse() as FtpWebResponse)
+            {
+                Stream responseStream = ResponseFile.GetResponseStream();
+
+                using (StreamReader reader = new StreamReader(responseStream))
+                {
+                    resDataString = reader.ReadToEnd();
+                }
+            }
+
+            return resDataString;
+        }
+
+        public IniFile LoadIni()
+        {
+            IniFile ini = new IniFile();
+
+            ini.Load("");
+
+            return ini;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -62,6 +135,16 @@ namespace AutoUpdate
         }
 
         private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
         {
 
         }
